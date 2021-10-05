ebbf9f1a29c401375dcabd30c75f2bcd6641153b
Alvin Jangvik <alvin.jangvik@futuregames.nu>
2021-10-05 14:59:47 +0200

    Made the beat tracker more precise

--- DiscoFixer/Assets/Scripts/Music/BeatTracker.cs
+++ DiscoFixer/Assets/Scripts/Music/BeatTracker.cs
@@ -6,11 +6,13 @@ using UnityEngine;
 public class BeatTracker : MonoBehaviour
 {
     [SerializeField] private AudioClip _song;
+    [SerializeField, Range(1, 10)] private int _skippedBeats = 1;
+    
     
     // boots and pants, bpm = 144
     private int _bpm = 144;
     private float _beat;
-    private float _songOffset = 0.2f;
+    private float _songOffset = 0.17f;
     private float _songPosition;
 
     private float _dspTimeSong;
@@ -18,7 +20,7 @@ public class BeatTracker : MonoBehaviour
     private float _lastBeat = 0;
 
     private AudioSource _audioPlayer;
-    // test
+    
     private void Start()
     {
         _beat = (60 * 100 / _bpm * 100) * 0.0001f;
@@ -32,10 +34,11 @@ public class BeatTracker : MonoBehaviour
     {
         _songPosition = (float) (AudioSettings.dspTime - _dspTimeSong) * _audioPlayer.pitch - _songOffset;
 
+        // What happens every beat
         if (_songPosition > _lastBeat + _beat)
         {
             print("beat");
-            _lastBeat += _beat * 2;
+            _lastBeat += _beat * _skippedBeats;
         }
     }
 }
