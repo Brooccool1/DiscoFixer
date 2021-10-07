d8520e56c40062966f5e1cf8f008266c7a39cedb
Lukas RH <51905473+HatarWienerbrods@users.noreply.github.com>
2021-10-07 14:24:10 +0200

    added Fx for tile prefab

--- DiscoFixer/Assets/Scripts/GridRelated/Tile.cs
+++ DiscoFixer/Assets/Scripts/GridRelated/Tile.cs
@@ -1,19 +1,24 @@
 using System;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.VFX;
 
 
-    public class Tile : MonoBehaviour
+public class Tile : MonoBehaviour
     {
         //public Color color;
         public int stages = 9;
         public int state = 9;
         public bool isBreaking = false;
         public bool isBroken = false;
+        public bool previousIsBreaking = false;
+        private VisualEffect vfx;
+        
 
         private void Start()
         {
             GameEvents.beat.onBeat += Break;
+           vfx = GetComponentInChildren<VisualEffect>();
         }
 
         private void Break()
@@ -21,6 +26,7 @@ using UnityEngine;
             if (isBreaking)
             {
                 state--;
+                previousIsBreaking = true;
             }
 
             if (state == 0)
@@ -40,5 +46,15 @@ using UnityEngine;
             {
                 GetComponent<SpriteRenderer>().material.color = Color.black;
             }
-        }
+
+            if (!isBreaking && previousIsBreaking)
+            {
+                
+                vfx.SetVector3("Color", new Vector3(50,1,1));
+                vfx.Play();
+                previousIsBreaking = false;
+            }
+
+
+    }
     }
