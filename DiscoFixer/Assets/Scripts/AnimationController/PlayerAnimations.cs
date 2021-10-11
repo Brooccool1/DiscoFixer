49094060b3b04b1f02431b00fec386dfe9002511
Alvin Jangvik <alvin.jangvik@futuregames.nu>
2021-10-11 11:04:06 +0200

    Player use the directional sprites

--- DiscoFixer/Assets/Scripts/AnimationController/PlayerAnimations.cs
+++ DiscoFixer/Assets/Scripts/AnimationController/PlayerAnimations.cs
@@ -6,16 +6,42 @@ public class PlayerAnimations : MonoBehaviour
 {
     [SerializeField] private List<Sprite> _sprites;
     private SpriteRenderer _sprite;
+    private Vector2 _direction = Vector2.zero;
+
+    private int _currentSprite = 0;
+    
+    private Vector2[] _directionToAnimation = new Vector2[8]
+    {
+        new (0, 1),
+        new (1, 1),
+        new (1, 0),
+        new (1, -1),
+        new (0, -1),
+        new (-1, -1),
+        new (-1, 0),
+        new (-1, 1)
+    };
     
     void Start()
     {
         _sprite = GetComponentInChildren<SpriteRenderer>();
     }
-
     
     void Update()
     {
-        
-        Player.direction
+        _direction = Player.direction;
+        _directionConverter();
+        _sprite.sprite = _sprites[_currentSprite];
+    }
+
+    private void _directionConverter()
+    {
+        for (int i = 0; i < _directionToAnimation.Length; i++)
+        {
+            if (_direction == _directionToAnimation[i])
+            {
+                _currentSprite = i;
+            }
+        }
     }
 }
