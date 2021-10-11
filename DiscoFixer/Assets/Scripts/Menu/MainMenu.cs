7072a68a0c5cba76c7e00e974db7a55238abb37f
Alice Tarkanmäki Bohman <nd.ag@hotmail.com>
2021-10-11 16:09:36 +0200

    Added a basic main menu with options menu and saving/loading audio

---
+++ DiscoFixer/Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class MainMenu : MonoBehaviour
+{
+
+
+    [Header("Different Menus")]
+    [SerializeField] private GameObject mainMenu;
+    [SerializeField] private GameObject optionsMenu;
+
+    [Header("Volume")]
+    [SerializeField] private AudioSource audioPlayer;
+    [SerializeField] private Slider volumeSlider;
+    [SerializeField] private TextMeshProUGUI currentVolume;
+
+
+    private void Start()
+    {
+        if(!PlayerPrefs.HasKey("AudioVolume"))
+            PlayerPrefs.SetFloat("AudioVolume", audioPlayer.volume);
+
+        OpenMainMenu();
+        volumeSlider.value = audioPlayer.volume;
+        audioPlayer.Play();
+    }
+
+    public void OpenSettings()
+    {
+        optionsMenu.SetActive(true);
+        mainMenu.SetActive(false);
+    }
+
+    public void OpenMainMenu()
+    {
+        optionsMenu.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void VolumeChange(float volume)
+    {
+        currentVolume.text = (volume * 100).ToString("0.00");
+        audioPlayer.volume = volume;
+        PlayerPrefs.SetFloat("AudioVolume", volume);
+    }
+
+}
