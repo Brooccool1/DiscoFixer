d92e469dc5b50e121c440703cf7aed73aec442db
Alvin Jangvik <alvin.jangvik@futuregames.nu>
2021-10-13 11:00:02 +0200

    Pause menu is now a menu

--- DiscoFixer/Assets/Scripts/Menu/PauseMenu.cs
+++ DiscoFixer/Assets/Scripts/Menu/PauseMenu.cs
@@ -2,27 +2,65 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour
 {
     [SerializeField] private GameObject _canvas;
     private float _timeScale = 1;
+    
+    [Header("Volume")]
+    [SerializeField] private AudioSource audioPlayer;
+    [SerializeField] private Text currentVolume;
+    [SerializeField] private bool _notParent = false;
 
-    void Update()
+    private void Start()
     {
-        if (Input.GetKeyDown(KeyCode.P))
-        {
-            _timeScale = _timeScale > 0 ? 0 : 1;
-        }
-        Time.timeScale = _timeScale;
-
-        if (Time.timeScale < 1)
-        {
-            _canvas.SetActive(true);
-        }
-        else
+        if (!_notParent)
         {
             _canvas.SetActive(false);
         }
+    }
+
+    void Update()
+    {
+        if (!_notParent)
+        {
+            if (Input.GetKeyDown(KeyCode.P))
+            {
+                _timeScale = _timeScale > 0 ? 0 : 1;
+
+                Time.timeScale = _timeScale;
+
+                if (Time.timeScale < 1)
+                {
+                    _canvas.SetActive(true);
+                }
+                else
+                {
+                    _canvas.SetActive(false);
+                }
+            }
+        }
+        print("time: " + Time.timeScale);
+    }
+
+    public void ContinueButton()
+    {
+        Time.timeScale = 1;
+        _canvas.SetActive(false);
+    }
+    
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    
+    public void VolumeChange(float volume)
+    {
+        currentVolume.text = "Volume: " + (volume * 100).ToString("0.0");
+        audioPlayer.volume = volume;
+        PlayerPrefs.SetFloat("AudioVolume", volume);
     }
 }
