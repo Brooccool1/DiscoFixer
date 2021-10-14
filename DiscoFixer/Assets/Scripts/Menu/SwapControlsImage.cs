be054b7089335c9e848def8b643f39a0898d2c67
Alice Tarkanmäki Bohman <nd.ag@hotmail.com>
2021-10-14 16:51:04 +0200

    Removed old code

--- DiscoFixer/Assets/Scripts/Menu/SwapControlsImage.cs
+++ DiscoFixer/Assets/Scripts/Menu/SwapControlsImage.cs
@@ -8,8 +8,6 @@ public class SwapControlsImage : MonoBehaviour
 {
     public int swapTimer;
     public Image image;
-    public Sprite imageSprite1;
-    public Sprite imageSprite2;
     public Sprite[] sprites;
     private float timeUntilSwap;
     int currentSprite = 0;
